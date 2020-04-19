9753a3476640d1251a92e5b315ce087125331769
PBlancoVW <50054131+PBlancoVW@users.noreply.github.com>
2020-04-18 20:01:16 -0400

    MFC Demo: parent window paramenter

--- MFCWindowDemo/MFCWindowDemo/source/MyFrame.cpp
+++ MFCWindowDemo/MFCWindowDemo/source/MyFrame.cpp
@@ -3,5 +3,5 @@
 #include "MyFrame.h"
 
 CMyFrame::CMyFrame() {
-	Create(NULL, _T("MFC Application Tutorial"), WS_SYSMENU, CRect(90, 120, 550, 480));
+	Create(NULL, _T("MFC Application Tutorial"), WS_SYSMENU, CRect(90, 120, 550, 480), NULL);
 }
